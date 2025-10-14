a1d0ee648e14ec4c83362dbb4c60efd515c62988
mate1821 <mate12185@gmail.com>
2025-10-13 22:47:04 -0500

    Design::Create Publisher.java(#111)

---
+++ src/main/java/com.example.greenet/Publisher.java
@@ -0,0 +1,22 @@
+package com.example.padron_decorador_modificado;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Publisher {
+    protected List<Suscribe> suscriptores = new ArrayList<>();
+
+    public void suscribir(Suscribe s) {
+        suscriptores.add(s);
+    }
+
+    public void eliminar(Suscribe s) {
+        suscriptores.remove(s);
+    }
+
+    protected void notificarSubs(String mensaje) {
+        for (Suscribe s : suscriptores) {
+            s.actualizar(mensaje);
+        }
+    }
+}
