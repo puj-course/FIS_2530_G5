05e05e2359418227c84267f097b6aecbff7daa70
mate1821 <mate12185@gmail.com>
2025-10-15 15:19:42 -0500

    Update Admin.java(#111)

--- src/main/java/com.example.greenet/Admin.java
+++ src/main/java/com.example.greenet/Admin.java
@@ -3,11 +3,13 @@ package com.example.padron_decorador_modificado;
 public class Admin implements Suscribe {
     private String nombre;
     private String correo;
+    private long telefono;
 
 
-    public Admin(String nombre, String correo) {
+    public Admin(String nombre, String correo, long telefono) {
         this.nombre = nombre;
         this.correo = correo;
+        this.telefono=telefono;
     }
 
     @Override
@@ -15,7 +17,7 @@ public class Admin implements Suscribe {
         Notificacion notificacion = new NotificacionBase();
         Notificacion notificacionWhatsApp = new WhatsAppDecorador(notificacion);
         Notificacion notificacionGmail = new GmailDecorador(notificacionWhatsApp);
-        notificacionGmail.sendMessage(mensaje,correo);
+        notificacionGmail.sendMessage(mensaje,correo,telefono);
 
     }
 }
