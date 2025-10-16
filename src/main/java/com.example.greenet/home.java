21667a62f1e6d212ace007cfd0d1df7d6af36ad1
mate1821 <mate12185@gmail.com>
2025-10-15 20:51:15 -0500

    Design:Create home.java(#103)

---
+++ src/main/java/com.example.greenet/home.java
@@ -0,0 +1,47 @@
+package com.example.pantalla_inicio;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+
+public class home {
+
+    @FXML
+    private Button buscarButton;
+
+    @FXML
+    private Button publicarButton;
+
+    @FXML
+    private void onGoToBuscar() {
+        System.out.println("Botón 'Buscar' presionado");
+      
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/pantalla_inicio/upload_material.fxml"));
+            Scene scene = new Scene(loader.load());
+            Stage stage = (Stage) buscarButton.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Buscar");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onGoToPublicaciones() {
+        System.out.println("Botón 'Publicar' presionado");
+     
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/pantalla_inicio/ProductSearch.fxml"));
+            Scene scene = new Scene(loader.load());
+            Stage stage = (Stage) publicarButton.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Publicaciones");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
