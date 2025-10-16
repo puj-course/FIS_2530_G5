b27c9d96c0b55bc809aebc129fff916623c7b4f8
S4muelRey06 <samuelreyparaps4@gmail.com>
2025-10-15 23:37:18 -0500

    Fix: Cambiar imagen de byte[] a String en PublicacionTecnología (#109)

--- src/main/java/com.example.greenet/PublicacionTecnologia.java
+++ src/main/java/com.example.greenet/PublicacionTecnologia.java
@@ -5,7 +5,7 @@ public class PublicacionTecnologia implements Publicacion {
     private String titulo;
     private String descripcion;
     private String categoria = "Tecnología";
-    private byte[] imagen;
+    private String imagen;
     private int publicadorId;
     private List<String> etiquetas = new ArrayList<>();
     
@@ -14,7 +14,7 @@ public class PublicacionTecnologia implements Publicacion {
     private String marca;
     private boolean garantia;
 
-    public PublicacionTecnologia(String titulo, String descripcion, byte[] imagen, 
+    public PublicacionTecnologia(String titulo, String descripcion, String imagen, 
                                int publicadorId, String modelo, String marca, boolean garantia) {
         this.titulo = titulo;
         this.descripcion = descripcion;
@@ -29,7 +29,7 @@ public class PublicacionTecnologia implements Publicacion {
     @Override public String getTitulo() { return titulo; }
     @Override public String getDescripcion() { return descripcion; }
     @Override public String getCategoria() { return categoria; }
-    @Override public byte[] getImagen() { return imagen; }
+    @Override public String getImagen() { return imagen; }
     @Override public int getPublicadorId() { return publicadorId; }
     @Override public List<String> getEtiquetas() { return etiquetas; }
     @Override public void setEtiquetas(List<String> etiquetas) { this.etiquetas = etiquetas; }
