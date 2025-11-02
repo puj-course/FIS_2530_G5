96f7ad62830d5f9438e9b312a6e2a1c3c511d5d5
S4muelRey06 <samuelreyparaps4@gmail.com>
2025-11-01 22:43:06 -0500

    Feat: Aplicar patrón fachada en la pantalla de vista previa (#128)

--- src/main/java/com.example.greenet/PreviewPostController.java
+++ src/main/java/com.example.greenet/PreviewPostController.java
@@ -4,7 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import services.FachadaImagen;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -14,20 +18,38 @@ public class PreviewPostController {
     private Button backButton;
 
     @FXML
-    private void onBack(ActionEvent event) {
-        Stage stage = (Stage) backButton.getScene().getWindow();
-        stage.close();
-    }
-    
-    @FXML
     private Label dateLabel;
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    private Label authorLabel;
+
+    @FXML
+    private Label descriptionLabel;
+
+    @FXML
+    private ImageView imageView;
 
     @FXML
     public void initialize() {
         LocalDate hoy = LocalDate.now();
-
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
         dateLabel.setText("Fecha: " + hoy.format(formato));
+    }
+
+    @FXML
+    private void onBack(ActionEvent event) {
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public void cargarPublicacion(String titulo, String autor, String descripcion, String imagenBase64) {
+        titleLabel.setText(titulo);
+        authorLabel.setText("Publicado por: " + autor);
+        descriptionLabel.setText(descripcion);
+        Image img = FachadaImagen.convertirBytesAImage(FachadaImagen.decodificarStringAImagen(imagenBase64));
+        imageView.setImage(img);
     }
 }
